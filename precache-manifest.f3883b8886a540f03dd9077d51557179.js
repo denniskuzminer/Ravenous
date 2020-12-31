4feb58afea2e0d5d39edebe1edf4f94e97ebee50
Dennis Kuzminer <denniskuzminer@gmail.com>
2020-12-31 14:24:47 -0500

    Updates

---
+++ precache-manifest.f3883b8886a540f03dd9077d51557179.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cad35924286aed8be68e8115194eee5",
+    "url": "/Ravenous/index.html"
+  },
+  {
+    "revision": "4b3b442c4998e1009980",
+    "url": "/Ravenous/static/css/main.0a2469af.chunk.css"
+  },
+  {
+    "revision": "40a536335e184874c60b",
+    "url": "/Ravenous/static/js/2.ff10b991.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ravenous/static/js/2.ff10b991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b3b442c4998e1009980",
+    "url": "/Ravenous/static/js/main.5ae3dd1a.chunk.js"
+  },
+  {
+    "revision": "cd33480439a159f358bd",
+    "url": "/Ravenous/static/js/runtime-main.9bb2f265.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Ravenous/static/media/logo.5d5d9eef.svg"
+  }
+]);
